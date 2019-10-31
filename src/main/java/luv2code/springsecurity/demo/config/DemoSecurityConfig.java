edfdc7ce353f70b7fa1edf5bdbfbc7b200abf167
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2019-10-31 10:30:40 -0500

    Added and wired in custom login form

--- src/main/java/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,5 +21,19 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter  {
                 .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("mary").password("test123").roles("MANAGER"))
                 .withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests() // Restrict access based on HttpServletRequest
+                .anyRequest().authenticated() // Any request to the app must be authenticated (ie. Logged in)
+                .and()
+                .formLogin() // Start customizing form login process
+                .loginPage("/showMyLoginPage") // Page that shows custom login form
+                .loginProcessingUrl("/authenticateTheUser") // Page that processes custom login form submission
+                                                            // Spring will handle this routing, you do not need to create
+                                                            // a controller request mapping for this.
+                                                            // Spring will also check the user id and password automatically
+                .permitAll(); // allow anyone to see the login page without logging in first
     }
 }
